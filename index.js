9f2f40223b5a910179a9cc4db425bfc7965a782e
Mike Wilson <mrmwilson@live.co.uk>
2020-08-28 20:04:57 +0100

    aadded colorChange function

--- index.js
+++ index.js
@@ -19,10 +19,23 @@ function ButtonPress() {
     var upperRangeValue = upperRangeElement.value;
 
     var randomNumber = GetRandomNumber(lowerRangeValue, upperRangeValue);
-
-    //call a function that changes the color of the randomNumberText 
-    //step 1 : create a new function to call (needs a paramater)
-    //step 2: create if - else logic and change the colour depending on the value passed in
+    
+    ColorChange(randomNumber,randomNumberText);
 
     randomNumberText.textContent = randomNumber;
 }
+
+   //call a function that changes the color of the randomNumberText 
+    //step 1 : create a new function to call (needs a paramater)
+    //step 2: create if - else logic and change the colour depending on the value passed in
+
+    function ColorChange(Number, HTMLElement){
+if (Number > 100) {
+HTMLElement.classList.add("high-number");
+HTMLElement.classList.remove("low-number");
+    
+} else {
+    HTMLElement.classList.remove("high-number");
+    HTMLElement.classList.add("low-number");
+}
+    }
