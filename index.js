868dbc992d70b0da5de8487a9be99aa13a8e3fb2
Mike Wilson <mrmwilson@live.co.uk>
2020-08-28 19:51:50 +0100

    fixed mumber generator

--- index.js
+++ index.js
@@ -4,12 +4,23 @@ window.onload = function () {
 }
 
 function GetRandomNumber (lowerRange,upperRange){
-    var randomNumber = Math.floor(Math.random() * upperRange) + lowerRange;
+    var randomNumber = Math.floor(Math.random() * (parseInt(upperRange) - parseInt(lowerRange) + 1) + parseInt(lowerRange));
+    
     return randomNumber;
 }
 
 function ButtonPress (){
     var randomNumberText = document.getElementById("random-number-text");
-    var randomNumber = GetRandomNumber(1,10);
+
+    var lowerRangeElement = document.getElementById("lower-range");
+    var upperRangeElement = document.getElementById("upper-range");
+    
+    var lowerRangeValue = lowerRangeElement.value;
+    var upperRangeValue = upperRangeElement.value;
+    
+
+
+
+    var randomNumber = GetRandomNumber(lowerRangeValue,upperRangeValue);
     randomNumberText.textContent = randomNumber;
 }
