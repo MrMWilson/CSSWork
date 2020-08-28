4c3210ac7f7d3322cd2caacd3a2694568c770dd8
JPHumphreys <Jonathan.Humphreys@academytrainee.com>
2020-08-28 19:55:02 +0100

    added css and comments

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
 window.onload = function () {
     console.log("test");
-    
+
 }
 
-function GetRandomNumber (lowerRange,upperRange){
+function GetRandomNumber(lowerRange, upperRange) {
     var randomNumber = Math.floor(Math.random() * (parseInt(upperRange) - parseInt(lowerRange) + 1) + parseInt(lowerRange));
-    
+
     return randomNumber;
 }
 
-function ButtonPress (){
+function ButtonPress() {
     var randomNumberText = document.getElementById("random-number-text");
 
     var lowerRangeElement = document.getElementById("lower-range");
     var upperRangeElement = document.getElementById("upper-range");
-    
+
     var lowerRangeValue = lowerRangeElement.value;
     var upperRangeValue = upperRangeElement.value;
-    
 
+    var randomNumber = GetRandomNumber(lowerRangeValue, upperRangeValue);
 
+    //call a function that changes the color of the randomNumberText 
+    //step 1 : create a new function to call (needs a paramater)
+    //step 2: create if - else logic and change the colour depending on the value passed in
 
-    var randomNumber = GetRandomNumber(lowerRangeValue,upperRangeValue);
     randomNumberText.textContent = randomNumber;
 }
